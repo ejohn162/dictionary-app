a1464e6dac60755edc7b1b6cd41b7d1b38910748
Elizabeth <ecjohnson891@gmail.com>
2022-04-09 10:09:09 -0600

    added logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import logo from "./logo.png";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-       Hello
+        <img src={logo} className="App-logo" alt="logo" />
+
+
       </header>
     </div>
   );
