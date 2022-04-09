e89c9ad8f42908b017535d0c51f5e917b8c67222
Elizabeth <ecjohnson891@gmail.com>
2022-04-09 10:27:07 -0600

    updated logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.png";
+import logo4 from "./logo4.png";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo4} className="App-logo" alt="logo" />
 
 
       </header>
