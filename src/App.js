20220ccc60b5096382e469617e2a1cbfb7a0a44f
Elizabeth <ecjohnson891@gmail.com>
2022-04-09 10:51:56 -0600

    added search input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo4 from "./logo4.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className ="container">
       <header className="App-header">
         <img src={logo4} className="App-logo" alt="logo" />
+     </header>
+     <main>
+       <Dictionary />
+     </main>
 
 
-      </header>
+    <footer className= "App-footer">
+      <small>
+      Coded by Elizabeth Johnson
+      </small>
+      </footer>
+     </div>
     </div>
   );
 }
